314f9f7d858139f2cc79b8aa7349e48e893b9518
D-VJAY <digvijayworking@gmail.com>
2024-02-08 18:23:33 +0530

    UpdateYml

--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    
+  <React.StrictMode>  
     <App />
-  
   </React.StrictMode>
 );
 
